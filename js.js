9ab50f00469d9ea94a6c49c104c0d96603e0bdbf
Hoang Duc <duccoc98@gmail.com>
2020-09-15 13:34:09 +0700

    2

---
+++ js.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
+let width = parseInt("20");
+let height = parseInt("10");
+let area = width * height;
+document.write("The area is: " + area);
